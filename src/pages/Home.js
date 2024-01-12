864287101c74060c92cdb3aa365ebe7d63f9d54d
Harsh-deepsingh <harshdeep.singh01@edu.sait.ca>
2024-01-12 03:57:19 -0700

    front-page setup

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import Navbar from "../features/Navbar/Navbar"
+
+import ProductList from "../features/product-list/ProductList.js"
+const Home = () => {
+  return (
+    <div>
+        <Navbar></Navbar>
+        <ProductList></ProductList>
+    </div>
+  )
+}
+
+export default Home
